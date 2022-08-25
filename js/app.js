69967453cc13c6836411a116358c4ad3e05d5d4b
loonhtet <gurdamanson@gmail.com>
2022-08-25 20:44:57 +0630

    25/8/22

--- js/app.js
+++ js/app.js
@@ -16,6 +16,37 @@ $(document).ready(function () {
 // Start Javascript Area
 
 // Start Students Conter Section
+
+var getcountervalues = document.querySelectorAll(".countervalues");
+
+getcountervalues.forEach(function (getcountervalues) {
+  // console.log(getcountervalues);
+
+  getcountervalues.textContent = 0;
+
+  const updatecounter = function () {
+    // console.log("work");
+
+    const getcttarget = +getcountervalues.getAttribute("data-target");
+    // console.log(getcttarget);
+    // console.log(typeof getcttarget);
+
+    const getctcontent = +getcountervalues.textContent;
+    // console.log(typeof getctcontent);
+
+    const incnumber = getcttarget / 100;
+    console.log(incnumber);
+
+    if (getctcontent < getcttarget) {
+      getcountervalues.textContent = getctcontent + incnumber;
+
+      setTimeout(updatecounter, 50);
+    }
+  };
+
+  updatecounter();
+});
+
 // End Students Conter Section
 
 // End Javascript Area
