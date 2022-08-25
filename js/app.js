b926e6a9df98e7d5f9bc3e27ec174ad15b1ea9d2
loonhtet <gurdamanson@gmail.com>
2022-08-25 17:36:47 +0630

    25/8/22

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Start Jquery Area
+$(document).ready(function () {
+  // Start Header
+  // Start Nav Bar
+
+  $(".navbuttons").click(function () {
+    $(this).toggleClass("crossxs");
+  });
+
+  // End Nav Bar
+  // End Header
+});
+
+// End Jquery Area
+
+// Start Javascript Area
+// End Javascript Area
