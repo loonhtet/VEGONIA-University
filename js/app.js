6f69e6047b18d192c4f1fb9c64792cf65972846c
loonhtet <gurdamanson@gmail.com>
2022-09-19 14:31:19 +0630

    19/9/22

--- js/app.js
+++ js/app.js
@@ -49,4 +49,40 @@ getcountervalues.forEach(function (getcountervalues) {
 
 // End Students Conter Section
 
+// Start Rating Section
+
+// start google code for chart
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Myanmar", 8],
+    ["Thailand", 2],
+    ["Singapore", 4],
+    ["Indonesia", 2],
+    ["Srilanka", 8],
+  ]);
+
+  var options = {
+    title: "International Students",
+    // is3D: true,
+    // pieHole: 0.4,
+    width: 550,
+    height: 400,
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
+
+// end google code for chart
+
+// End Rating Section
+
 // End Javascript Area
